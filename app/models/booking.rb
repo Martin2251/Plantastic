45358f52f3157a4cf4ae3836fbf451eaed236a45
Ayan Ali <ayanmohamud.ali@hotmail.com>
2020-08-08 16:24:14 +0100

    search function implemented

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,16 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :plant
+  include PgSearch::Model
+  pg_search_scope :search_by_plant_name,
+    against: [ :beginning_date, :end_date ],
+    associated_against: {
+      plant: [ :name, :category]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
+
   has_many :reviews, dependent: :destroy
+
 end
