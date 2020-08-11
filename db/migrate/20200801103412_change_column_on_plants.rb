bfb51cbffc53c5667cf3b457ecb6fd0701f816a9
nouria chaouy <nouria.chaouy@gmail.com>
2020-08-11 22:54:00 +0100

    seeding images

--- db/migrate/20200801103412_change_column_on_plants.rb
+++ db/migrate/20200801103412_change_column_on_plants.rb
@@ -2,5 +2,6 @@ class ChangeColumnOnPlants < ActiveRecord::Migration[6.0]
   def change
     remove_column :plants, :type, :string
     add_column :plants, :species, :string
+    add_column :plants, :image_url, :string
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,8 @@ file = URI.open('https://images.unsplash.com/photo-1459411552884-841db9b3cc2a?ix
     address: Faker::Address.full_address,
     description: Faker::Hipster.sentences,
     longitude: Faker::Address.longitude,
-    latitude: Faker::Address.latitude
+    latitude: Faker::Address.latitude,
+    image_url: 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1568042845-Cactus_mix_yellow_2048x.jpg?crop=0.838xw:0.838xh;0.0705xw,0.122xh'
   )
     # plant.photos.attach(io: file, filename: 'plant.png', content_type: 'image/png')
     plant.save
